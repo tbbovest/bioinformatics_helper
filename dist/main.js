4e6694b3c95965eacaab018b4ae36e96380affee
unknown <tbbovest@gmail.com>
2024-04-13 18:17:02 -0400

    Started terminal window and date/time

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,8 @@
 "use strict";
 const date_time = document.getElementById("date-time");
 var today = new Date().toLocaleString().replace(",", "").replace(/:.. /, " ");
+document.getElementById('terminal-back').style.color = "White";
+document.getElementById('terminal-back').style.font = "Helvetica, sans-serif";
+document.getElementById('terminal-back').style.fontWeight = "600";
+//document.getElementById('terminal-back')!.style.letterSpacing = "1px";
 date_time.textContent = today;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 const date_time = document.getElementById("date-time");
 
-
 var today = new Date().toLocaleString().replace(",","").replace(/:.. /," ");
+
+document.getElementById('terminal-back')!.style.color = "White";
+document.getElementById('terminal-back')!.style.font = "Helvetica, sans-serif";
+document.getElementById('terminal-back')!.style.fontWeight = "600";
+
 
 date_time!.textContent = today;
