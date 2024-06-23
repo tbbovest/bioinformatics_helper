2f2e5b268b58b318926b66391214fa49d2e19eb1
unknown <tbbovest@gmail.com>
2024-06-23 11:13:09 -0400

    UI fixes

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,7 @@
 "use strict";
 let date_time = document.getElementById('date-time');
+const firstButton = document.getElementById('firstoption');
+const secButton = document.getElementById('secoption');
 var today = new Date().toLocaleString().replace(",", "").replace(/:.. /, " ");
 date_time.textContent = today;
+firstButton.addEventListener("click", secButton.classList.add('display: none'));
